9a2df696280869f366c0b3d6d53561594719000d
Gustaf Mossakowski <web@koenige.org>
2017-09-20 07:45:58 +0000

    contacts: bugfix export definition; add geojson export fields; add first lat/lon of addresses as lat/lon export
    git-svn-id: file:///Repos/modules/contacts/trunk@24 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_tables/contacts.php
+++ zzbrick_tables/contacts.php
@@ -18,11 +18,13 @@ $zz['table'] = '/*_PREFIX_*/contacts';
 $zz['fields'][1]['title'] = 'ID';
 $zz['fields'][1]['field_name'] = 'contact_id';
 $zz['fields'][1]['type'] = 'id';
+$zz['fields'][1]['geojson'] = 'id';
 
 $zz['fields'][2]['field_name'] = 'contact';
 $zz['fields'][2]['type'] = 'memo';
 $zz['fields'][2]['trim'] = true;
 $zz['fields'][2]['rows'] = 2;
+$zz['fields'][2]['geojson'] = 'title';
 $zz['fields'][2]['export_no_html'] = true;
 $zz['fields'][2]['unless']['export_mode']['list_format'] = 'nl2br';
 
@@ -31,6 +33,7 @@ $zz['fields'][3]['type'] = 'identifier';
 $zz['fields'][3]['fields'] = ['contact'];
 $zz['fields'][3]['conf_identifier']['exists'] = '-';
 $zz['fields'][3]['hide_in_list'] = true;
+$zz['fields'][3]['geojson'] = 'identifier';
 
 $zz['fields'][4]['title'] = 'Category';
 $zz['fields'][4]['field_name'] = 'contact_category_id';
@@ -41,6 +44,7 @@ $zz['fields'][4]['sql'] = sprintf('SELECT category_id, category
 	$zz_setting['category']['contact']
 );
 $zz['fields'][4]['display_field'] = 'category';
+$zz['fields'][4]['geojson'] = 'category';
 
 require __DIR__.'/addresses.php';
 $zz['fields'][5] = $zz_sub;
@@ -66,7 +70,7 @@ $zz['fields'][5]['unless']['export_mode']['subselect']['field_suffix'][2] = '<br
 $zz['fields'][5]['if']['export_mode']['subselect']['field_suffix'][2] = "\r";
 $zz['fields'][5]['unless']['export_mode']['list_append_next'] = true;
 $zz['fields'][5]['export_no_html'] = true;
-$zz['fields'][6]['if']['export_mode']['subselect']['concat_rows'] = "\r\r";
+$zz['fields'][5]['if']['export_mode']['subselect']['concat_rows'] = "\r\r";
 
 require __DIR__.'/contacts-details.php';
 $zz['fields'][6] = $zz_sub;
@@ -89,12 +93,22 @@ $zz['fields'][6]['export_no_html'] = true;
 
 $zz['fields'][7] = false; // contacts_verifications
 
+$zz['fields'][8]['field_name'] = 'latlon';
+$zz['fields'][8]['type'] = 'display';
+$zz['fields'][8]['exclude_from_search'] = true;
+$zz['fields'][8]['hide_in_form'] = true;
+$zz['fields'][8]['unless']['export_mode']['hide_in_list'] = true;
+$zz['fields'][8]['geojson'] = 'latitude/longitude';
+
 $zz['fields'][20]['title'] = 'Updated';
 $zz['fields'][20]['field_name'] = 'last_update';
 $zz['fields'][20]['type'] = 'timestamp';
 $zz['fields'][20]['hide_in_list'] = true;
 
 $zz['sql'] = 'SELECT /*_PREFIX_*/contacts.*, category
+		, (SELECT CONCAT(latitude, ",", longitude) FROM /*_PREFIX_*/addresses
+			WHERE /*_PREFIX_*/addresses.contact_id = /*_PREFIX_*/contacts.contact_id
+			LIMIT 1) AS latlon
 	FROM /*_PREFIX_*/contacts
 	LEFT JOIN /*_PREFIX_*/categories
 		ON /*_PREFIX_*/contacts.contact_category_id = /*_PREFIX_*/categories.category_id
@@ -112,4 +126,4 @@ $zz['filter'][1]['type'] = 'list';
 $zz['filter'][1]['where'] = 'contact_category_id';
 $zz['filter'][1]['field_name'] = 'contact_category_id';
 
-$zz_conf['export'] = 'CSV Excel';
+$zz_conf['export'][] = 'CSV Excel';
