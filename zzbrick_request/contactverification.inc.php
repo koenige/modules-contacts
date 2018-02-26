837010ecf4c6d5b10534df3aa5951cec73b00b62
Gustaf Mossakowski <web@koenige.org>
2018-02-26 04:48:52 +0000

    add contact verification script with German and English templates based on newsletters module
    git-svn-id: file:///Repos/modules/contacts/trunk@39 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

---
+++ zzbrick_request/contactverification.inc.php
@@ -0,0 +1,107 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Confirm contact registration
+ *
+ * http://www.zugzwang.org/modules/contact
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2015-2016, 2018 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+function mod_contacts_contactverification() {
+	global $zz_conf;
+	global $zz_setting;
+	global $zz_page;
+
+	$zz_setting['cache'] = false;
+	$zz_setting['extra_http_headers'][] = 'X-Frame-Options: Deny';
+	$zz_setting['extra_http_headers'][] = "Content-Security-Policy: frame-ancestors 'self'";
+
+	$form = [];
+	$form['reminder'] = false;
+	$form['own_e_mail'] = $zz_setting['own_e_mail'];
+	$url = parse_url($_SERVER['REQUEST_URI']);
+	$form['action'] = $url['path'];
+
+	$possible_actions = ['confirm', 'delete'];
+	$page['query_strings'] = ['code', 'action', 'confirm', 'delete'];
+
+	// What to do?
+	if (!empty($_GET['code']) && !empty($_GET['action'])
+		&& in_array($_GET['action'], $possible_actions)) {
+		$form['code'] = $_GET['code'];
+		$action = $_GET['action'];
+	} elseif (!empty($_GET['confirm'])) {
+		$form['code'] = $_GET['confirm'];
+		$action = 'confirm';
+	} elseif (!empty($_GET['delete'])) {
+		$form['code'] = $_GET['delete'];
+		$action = 'delete';
+	} elseif (!empty($_GET['code'])) {
+		$form['code'] = $_GET['code'];
+		$form['form'] = true;
+		$action = false;
+		$form['reminder'] = true;
+	} else {
+		$form['code'] = false;
+		$form['form'] = true;
+		$page['text'] = wrap_template('contact-verification', $form);
+		return $page;
+	}
+
+	$sql = 'SELECT contact_id, verification_date
+		FROM contacts_verifications
+		WHERE verification_hash = "%s"';
+	$sql = sprintf($sql, wrap_db_escape($form['code']));
+	$data = wrap_db_fetch($sql);
+	
+	if (!$data) {
+		$form['no_data'] = true;
+		$form['form'] = true;
+		$page['text'] = wrap_template('contact-verification', $form);
+		return $page;
+	}
+	
+	if ($data['verification_date']) {
+		if ($action === 'confirm') {
+			$form['already_confirmed'] = true;
+		} elseif ($action === 'delete') {
+			$form['confirmed_delete'] = true;
+		} else {
+			$form['form'] = true;
+		}
+		$page['text'] = wrap_template('contact-verification', $form);
+		return $page;
+	}
+	
+	if (!$action) {
+		$page['text'] = wrap_template('contact-verification', $form);
+		return $page;
+	}
+
+	require_once $zz_conf['dir'].'/zzform.php';
+	$values = [];
+	$values['POST']['contact_id'] = $data['contact_id'];
+	if ($action === 'confirm') {
+		$values['action'] = 'update';
+		$values['POST']['verification_date'] = date('Y-m-d H:i:s');;
+		$values['POST']['verification_ip'] = $_SERVER['REMOTE_ADDR'];
+	} else {
+		$values['action'] = 'delete';
+	}
+	$ops = zzform_multi('contacts_verifications', $values);
+	if (!$ops['id']) {
+		wrap_error(sprintf(
+			'Registration ID %s could not be %sd. %s',
+			$data['contact_id'], $values['action'], implode(', ', $ops['error'])
+		), E_USER_ERROR);
+	}
+	$form[$action] = true;
+
+	$page['text'] = wrap_template('contact-verification', $form);
+	return $page;
+}
