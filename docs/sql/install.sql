dc110e5572ae9937d8ba4098491aa9a76167e9d2
Gustaf Mossakowski <gustaf@koenige.org>
2020-04-27 15:27:14 +0000

    update install.sql to current database structure
    git-svn-id: file:///Repos/modules/contacts/trunk@107 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- docs/sql/install.sql
+++ docs/sql/install.sql
@@ -1,3 +1,15 @@
+/**
+ * Zugzwang Project
+ * SQL for installation of contacts module
+ *
+ * http://www.zugzwang.org/modules/contacts
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2018-2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
 SET NAMES utf8mb4;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
@@ -5,18 +17,20 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `addresses`;
 CREATE TABLE `addresses` (
-  `address_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contact_id` int(10) unsigned NOT NULL,
-  `address` text COLLATE utf8mb4_unicode_ci,
-  `postcode` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `place` varchar(127) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `country_id` int(10) unsigned NOT NULL,
+  `address_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` int unsigned NOT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `postcode` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `place` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country_id` int unsigned NOT NULL,
   `latitude` decimal(9,6) DEFAULT NULL,
   `longitude` decimal(10,6) DEFAULT NULL,
-  `address_category_id` int(10) unsigned DEFAULT NULL,
-  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `address_category_id` int unsigned NOT NULL,
+  `receive_mail` enum('yes','no') CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`address_id`),
   KEY `contact_id` (`contact_id`),
+  KEY `receive_mail` (`receive_mail`),
   KEY `country_id` (`country_id`),
   KEY `address_category_id` (`address_category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -24,28 +38,29 @@ CREATE TABLE `addresses` (
 
 DROP TABLE IF EXISTS `contactdetails`;
 CREATE TABLE `contactdetails` (
-  `contactdetail_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contact_id` int(10) unsigned NOT NULL,
-  `identification` varchar(127) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `provider_category_id` int(10) unsigned NOT NULL,
-  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `contactdetail_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` int unsigned NOT NULL,
+  `identification` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `provider_category_id` int unsigned NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`contactdetail_id`),
-  KEY `provider_category_id` (`provider_category_id`),
-  KEY `contact_id` (`contact_id`)
+  KEY `contact_id` (`contact_id`),
+  KEY `provider_category_id` (`provider_category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 DROP TABLE IF EXISTS `contacts`;
 CREATE TABLE `contacts` (
-  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `contact_short` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `contact_abbr` varchar(8) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `identifier` varchar(63) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci,
-  `remarks` text COLLATE utf8mb4_unicode_ci,
-  `contact_category_id` int(10) unsigned NOT NULL,
-  `parameters` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contact_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_short` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `contact_abbr` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `identifier` varchar(80) CHARACTER SET latin1 COLLATE latin1_general_ci DEFAULT NULL,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `remarks` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `contact_category_id` int unsigned NOT NULL,
+  `published` enum('yes','no') CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT 'no',
+  `parameters` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`contact_id`),
   UNIQUE KEY `identifier` (`identifier`),
@@ -55,34 +70,34 @@ CREATE TABLE `contacts` (
 
 DROP TABLE IF EXISTS `contacts_contacts`;
 CREATE TABLE `contacts_contacts` (
-  `cc_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contact_id` int(10) unsigned NOT NULL,
-  `main_contact_id` int(10) unsigned NOT NULL,
-  `relation_category_id` int(10) unsigned NOT NULL,
+  `cc_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` int unsigned NOT NULL,
+  `main_contact_id` int unsigned NOT NULL,
+  `relation_category_id` int unsigned NOT NULL,
   `remarks` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  `sequence` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `sequence` tinyint unsigned NOT NULL DEFAULT '1',
   `published` enum('yes','no') CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL DEFAULT 'no',
-  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`cc_id`),
   UNIQUE KEY `contact_id` (`contact_id`,`main_contact_id`),
   KEY `main_contact_id` (`main_contact_id`),
   KEY `relation_category_id` (`relation_category_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
 DROP TABLE IF EXISTS `contacts_verifications`;
 CREATE TABLE `contacts_verifications` (
-  `cv_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `contact_id` int(10) unsigned NOT NULL,
+  `cv_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `contact_id` int unsigned NOT NULL,
   `entry_date` datetime NOT NULL,
   `entry_ip` varbinary(16) NOT NULL,
   `verification_date` datetime DEFAULT NULL,
   `verification_ip` varbinary(16) DEFAULT NULL,
   `verification_hash` varchar(8) CHARACTER SET latin1 COLLATE latin1_general_cs DEFAULT NULL,
-  `confirmed_mail` varchar(127) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `mails_sent` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `confirmed_mail` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mails_sent` tinyint unsigned NOT NULL DEFAULT '0',
   `status` enum('unverified','confirmed per link','confirmed manually','complete','unsubscribed','deleted') CHARACTER SET latin1 COLLATE latin1_general_cs NOT NULL,
-  `language_id` int(10) unsigned NOT NULL,
+  `language_id` int unsigned NOT NULL,
+  `completion_date` datetime DEFAULT NULL,
   PRIMARY KEY (`cv_id`),
   UNIQUE KEY `contact_id` (`contact_id`),
   UNIQUE KEY `verification_hash` (`verification_hash`),
