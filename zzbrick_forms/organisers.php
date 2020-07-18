a08ec0c1e9eceb555d0426f354bfa367e0b5208a
Gustaf Mossakowski <gustaf@koenige.org>
2020-07-18 13:51:50 +0000

    add forms for organisers and places, to be used with events module
    git-svn-id: file:///Repos/modules/contacts/trunk@124 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

---
+++ zzbrick_forms/organisers.php
@@ -0,0 +1,27 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Form for places
+ *
+ * http://www.zugzwang.org/modules/contacts
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+$zz = zzform_include_table('contacts');
+$zz['title'] = 'Organisers';
+
+$zz['sql'] .= 'WHERE categories.parameters LIKE "%&event_organiser=1%"';
+
+$zz['fields'][4]['sql'] .= ' AND categories.parameters LIKE "%&event_organiser=1%"';
+
+$zz['filter'][1]['sql'] = 'SELECT category_id, category
+	FROM /*_PREFIX_*/contacts
+	LEFT JOIN /*_PREFIX_*/categories
+		ON /*_PREFIX_*/contacts.contact_category_id = /*_PREFIX_*/categories.category_id
+	WHERE categories.parameters LIKE "%&event_organiser=1%"
+	ORDER BY category';
