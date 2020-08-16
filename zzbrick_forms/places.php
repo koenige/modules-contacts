59a4407dcbeeb5f4844fa185812e64dd109d1761
Gustaf Mossakowski <gustaf@koenige.org>
2020-08-16 13:26:44 +0000

    places: remove contactdetails to the ones with parameter places=1, improve layout of table; restrict address categories to places=1, hide addresses.receive_mail if set; set published to yes; hide contacts.parameters
    git-svn-id: file:///Repos/modules/contacts/trunk@127 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_forms/places.php
+++ zzbrick_forms/places.php
@@ -12,12 +12,49 @@
  */
 
 
-$zz = zzform_include_table('contacts');
+$sql = 'SELECT category_id, category, parameters 
+	FROM categories
+	WHERE main_category_id = %d
+	AND parameters LIKE "%%&places=1%%"
+	ORDER BY sequence, path';
+$sql = sprintf($sql, wrap_category_id('provider'));
+$values['contactdetails'] = wrap_db_fetch($sql, 'category_id');
+
+$zz = zzform_include_table('contacts', $values);
 $zz['title'] = 'Places';
 
-$zz['sql'] .= 'WHERE categories.parameters LIKE "%&event_place=1%"';
+// contact
+$zz['fields'][2]['title'] = 'Place';
 
+// category
+$zz['fields'][4]['hide_in_list'] = true;
 $zz['fields'][4]['sql'] .= ' AND categories.parameters LIKE "%&event_place=1%"';
+
+// addresses
+$zz['fields'][5]['min_records'] = 1;
+$zz['fields'][5]['max_records'] = 1;
+if (!empty($zz['fields'][5]['fields'][10])) {
+	// receive_mail
+	$zz['fields'][5]['fields'][10]['type'] = 'hidden';
+	$zz['fields'][5]['fields'][10]['type_detail'] = 'enum';
+	$zz['fields'][5]['fields'][10]['value'] = 'yes';
+	$zz['fields'][5]['fields'][10]['hide_in_form'] = true;
+}
+$zz['fields'][5]['fields'][9]['sql'] = 'SELECT category_id, category, main_category_id
+	FROM categories
+	WHERE parameters LIKE "%&places=1%"
+	ORDER BY sequence, category';
+
+// published
+$zz['fields'][14]['type'] = 'hidden';
+$zz['fields'][14]['type_detail'] = 'enum';
+$zz['fields'][14]['value'] = 'yes';
+$zz['fields'][14]['hide_in_form'] = true;
+
+// parameters
+unset($zz['fields'][15]);
+
+$zz['sql'] .= 'WHERE categories.parameters LIKE "%&event_place=1%"';
 
 $zz['filter'][1]['sql'] = 'SELECT category_id, category
 	FROM /*_PREFIX_*/contacts
