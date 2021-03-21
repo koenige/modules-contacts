7c1fde2d6e4dc4b53f6fc61e53d8121e7901b60f
Gustaf Mossakowski <gustaf@koenige.org>
2021-03-21 17:15:13 +0000

    contacts: always define contacts.remarks in table definition
    git-svn-id: file:///Repos/modules/contacts/trunk@212 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_tables/contacts.php
+++ zzbrick_tables/contacts.php
@@ -240,7 +240,12 @@ $zz['fields'][12]['type'] = 'memo';
 $zz['fields'][12]['hide_in_list'] = true;
 $zz['fields'][12]['format'] = 'markdown';
 
-$zz['fields'][13] = [];  // remarks
+$zz['fields'][13]['field_name'] = 'remarks';
+$zz['fields'][13]['type'] = 'memo';
+$zz['fields'][13]['format'] = 'markdown';
+$zz['fields'][13]['hide_in_list'] = true;
+$zz['fields'][13]['merge_append'] = true;
+$zz['fields'][13]['rows'] = 3;
 
 if (!isset($values['relations'])) {
 	if (isset($values['relations_restrict_to']))
