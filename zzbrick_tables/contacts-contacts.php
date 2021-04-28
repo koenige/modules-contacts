b1a322a0803669bbf5db9afc2486e4cdb9f48679
Gustaf Mossakowski <gustaf@koenige.org>
2021-04-28 22:20:34 +0000

    contacts/contacts: if only value is possible for contact or main contact: do not set it automatically
    git-svn-id: file:///Repos/modules/contacts/trunk@224 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_tables/contacts-contacts.php
+++ zzbrick_tables/contacts-contacts.php
@@ -37,6 +37,7 @@ $zz['fields'][2]['sql'] = 'SELECT contact_id, contact, identifier
 $zz['fields'][2]['display_field'] = 'contact';
 $zz['fields'][2]['search'] = 'contacts.contact';
 $zz['fields'][2]['placeholder'] = true;
+$zz['fields'][2]['select_dont_force_single_value'] = true;
 
 $zz['fields'][4]['title'] = 'Relation';
 $zz['fields'][4]['field_name'] = 'relation_category_id';
@@ -61,6 +62,7 @@ $zz['fields'][3]['sql'] = 'SELECT contact_id, contact, identifier
 $zz['fields'][3]['display_field'] = 'main_contact';
 $zz['fields'][3]['search'] = 'main_contacts.contact';
 $zz['fields'][3]['character_set'] = 'utf8';
+$zz['fields'][3]['select_dont_force_single_value'] = true;
 
 $zz['fields'][11]['field_name'] = 'role';
 $zz['fields'][11]['type'] = 'text';
