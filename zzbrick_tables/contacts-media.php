d064f0ba0a99b2b4abaa02132721f3bdd6493f17
Gustaf Mossakowski <gustaf@koenige.org>
2021-05-04 16:24:42 +0000

    contacts-media: update table definition, re-arrange field order, do not hide sequence per default; group media by folder
    git-svn-id: file:///Repos/modules/contacts/trunk@225 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_tables/contacts-media.php
+++ zzbrick_tables/contacts-media.php
@@ -7,32 +7,30 @@
  * http://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2014-2015, 2017-2018, 2020 Gustaf Mossakowski
+ * @copyright Copyright © 2014-2015, 2017-2018, 2021 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
-$zz['title'] = 'Contacts/Media';
+$zz['title'] = 'Media for Contacts';
 $zz['table'] = '/*_PREFIX_*/contacts_media';
 
 $zz['fields'][1]['title'] = 'ID';
 $zz['fields'][1]['field_name'] = 'contact_medium_id';
 $zz['fields'][1]['type'] = 'id';
 
-$zz['fields'][2]['title'] = 'Contact';
+$zz['fields'][4]['title'] = 'No.';
+$zz['fields'][4]['field_name'] = 'sequence';
+$zz['fields'][4]['type'] = 'number';
+$zz['fields'][4]['auto_value'] = 'increment';
+$zz['fields'][4]['def_val_ignore'] = true;
+
 $zz['fields'][2]['field_name'] = 'contact_id';
 $zz['fields'][2]['type'] = 'select';
 $zz['fields'][2]['sql'] = 'SELECT contact_id, contact
 	FROM /*_PREFIX_*/contacts
-	ORDER BY contact';
+	ORDER BY identifier';
 $zz['fields'][2]['display_field'] = 'contact';
-
-$zz['fields'][4]['title'] = 'No.';
-$zz['fields'][4]['field_name'] = 'sequence';
-$zz['fields'][4]['type'] = 'hidden';
-$zz['fields'][4]['class'] = 'hidden';
-$zz['fields'][4]['value'] = 1;
-$zz['fields'][4]['def_val_ignore'] = true;
 
 $zz['fields'][5]['title'] = 'Preview';
 $zz['fields'][5]['field_name'] = 'image';
@@ -53,23 +51,33 @@ $zz['fields'][5]['path'] = [
 
 $zz['fields'][3]['title'] = 'Medium';
 $zz['fields'][3]['field_name'] = 'medium_id';
+$zz['fields'][3]['id_field_name'] = 'media.medium_id';
 $zz['fields'][3]['type'] = 'select';
-$zz['fields'][3]['sql'] = 'SELECT medium_id
+$zz['fields'][3]['sql'] = sprintf('SELECT /*_PREFIX_*/media.medium_id
+		, folders.title AS folder
 		, CONCAT("[", /*_PREFIX_*/media.medium_id, "] ", /*_PREFIX_*/media.title) AS image
 	FROM /*_PREFIX_*/media 
-	ORDER BY title';
+	LEFT JOIN /*_PREFIX_*/media folders
+		ON /*_PREFIX_*/media.main_medium_id = folders.medium_id
+	WHERE /*_PREFIX_*/media.filetype_id != %d
+	ORDER BY folders.title, /*_PREFIX_*/media.title', wrap_filetype_id('folder'));
+$zz['fields'][3]['sql_character_set'][1] = 'utf8';
+$zz['fields'][3]['sql_character_set'][2] = 'utf8';
 $zz['fields'][3]['display_field'] = 'image';
+$zz['fields'][3]['group'] = 'folder';
 $zz['fields'][3]['exclude_from_search'] = true;
 
 $zz['fields'][20]['field_name'] = 'last_update';
 $zz['fields'][20]['type'] = 'timestamp';
 $zz['fields'][20]['hide_in_list'] = true;
 
-$zz['subselect']['sql'] = 'SELECT contact_id, filename, extension, version
+$zz['subselect']['sql'] = 'SELECT contact_id, filename, t_mime.extension, version
 	FROM /*_PREFIX_*/contacts_media
 	LEFT JOIN /*_PREFIX_*/media USING (medium_id)
+	LEFT JOIN /*_PREFIX_*/filetypes AS o_mime USING (filetype_id)
 	LEFT JOIN /*_PREFIX_*/filetypes AS t_mime 
 		ON /*_PREFIX_*/media.thumb_filetype_id = t_mime.filetype_id
+	WHERE o_mime.mime_content_type = "image"
 ';
 $zz['subselect']['concat_fields'] = '';
 $zz['subselect']['field_suffix'][0] = '.'.$zz_setting['media_preview_size'].'.';
@@ -79,7 +87,7 @@ $zz['subselect']['suffix'] = '">';
 $zz['subselect']['dont_mark_search_string'] = true;
 
 $zz['sql'] = 'SELECT /*_PREFIX_*/contacts_media.*
-	, contact
+	, /*_PREFIX_*/contacts.contact
 	, CONCAT("[", /*_PREFIX_*/media.medium_id, "] ", /*_PREFIX_*/media.title) AS image
 	, /*_PREFIX_*/media.filename, /*_PREFIX_*/media.version
 	, t_mime.extension AS thumb_extension
@@ -91,4 +99,4 @@ $zz['sql'] = 'SELECT /*_PREFIX_*/contacts_media.*
 		ON /*_PREFIX_*/media.thumb_filetype_id = t_mime.filetype_id
 	WHERE o_mime.mime_content_type = "image"
 ';
-$zz['sqlorder'] = ' ORDER BY /*_PREFIX_*/contacts.contact DESC, media.sequence';
+$zz['sqlorder'] = ' ORDER BY /*_PREFIX_*/contacts.identifier DESC, /*_PREFIX_*/media.sequence';
