f74fea713d47dd6ab5ddccbfe6287aacfdce1934
Gustaf Mossakowski <gustaf@koenige.org>
2021-06-23 08:15:43 +0000

    contacts/contacts: add form for contacts linked to a main contact; if scope is given, restrict relations to this category; change title
    git-svn-id: file:///Repos/modules/contacts/trunk@253 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

---
+++ zzbrick_forms/contacts-contacts.php
@@ -0,0 +1,66 @@
+<?php 
+
+/**
+ * Zugzwang Project
+ * Form for contacts without persons
+ *
+ * http://www.zugzwang.org/modules/contacts
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+if (count($brick['vars']) !== 1) wrap_quit(404);
+
+$sql = 'SELECT contact_id, contact
+	FROM contacts
+	WHERE identifier = "%s"';
+$sql = sprintf($sql, wrap_db_escape($brick['vars'][0]));
+$contact = wrap_db_fetch($sql);
+if (!$contact) wrap_quit(404);
+
+if (!empty($brick['local_settings']['scope'])) {
+	$sql = 'SELECT category_id, category, parameters
+		FROM categories
+		WHERE main_category_id = %d
+		AND path LIKE "%%%s"';
+	$sql = sprintf($sql
+		, wrap_category_id('relation')
+		, $brick['local_settings']['scope']
+	);
+	$category = wrap_db_fetch($sql);
+	parse_str($category['parameters'], $category['parameters']);
+	if (!$category) wrap_quit(404);
+} else {
+	$category = [];
+}
+
+$zz = zzform_include_table('contacts-contacts');
+$zz['where']['main_contact_id'] = $contact['contact_id'];
+if ($category)
+	$zz['where']['relation_category_id'] = $category['category_id'];
+
+if (!$category)
+	$zz['title'] = $contact['contact'];
+else
+	$zz['title'] = sprintf('<a href="../">%s</a>:<br>%s'
+		, $contact['contact']
+		, !empty($category['parameters']['parents']['relation']) ? $category['parameters']['parents']['relation'] : $category['category']
+	);
+
+if (!empty($category['parameters']['contact']['add_details'])) {
+	$zz['fields'][2]['add_details'] = $category['parameters']['contact']['add_details'];
+}
+if (!empty($category['parameters']['contact']['category'])) {
+	$zz['fields'][2]['sql'] = wrap_edit_sql($zz['fields'][2]['sql'], 'WHERE',
+		sprintf('contact_category_id = %d', wrap_category_id('contact/'.$category['parameters']['contact']['category']))
+	);
+}
+
+$zz['fields'][6]['type'] = 'sequence';
+
+$zz['fields'][11]['size'] = 32;
+
+$zz_conf['referer'] = '../';
