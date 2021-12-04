ad8e7678087d213551db3626f339851906af1947
Gustaf Mossakowski <gustaf@koenige.org>
2021-12-04 07:44:59 +0000

    nice table name for contacts_contacts
    git-svn-id: file:///Repos/modules/contacts/trunk@287 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * contacts module
+ * configuration
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/contacts
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+$zz_conf['nice_tablename']['contacts_contacts'] = 'Linked Contacts';
