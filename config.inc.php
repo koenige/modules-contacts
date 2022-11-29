e0ab6e0375b3bd60a1b93b2258ee819be12df980
Gustaf Mossakowski <gustaf@koenige.org>
2022-11-29 14:49:19 +0000

    config: add nice tablename for Contact details
    git-svn-id: file:///Repos/modules/contacts/trunk@358 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- config.inc.php
+++ config.inc.php
@@ -8,9 +8,10 @@
  * https://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2021 Gustaf Mossakowski
+ * @copyright Copyright © 2021-2022 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
 $zz_conf['nice_tablename']['contacts_contacts'] = 'Linked Contacts';
+$zz_conf['nice_tablename']['contactdetails'] = 'Contact details';
