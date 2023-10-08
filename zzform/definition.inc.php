a1d49aff889738823d9ad181752db7d7c1ff3d49
Gustaf Mossakowski <gustaf@koenige.org>
2023-10-08 16:02:56 +0000

    zzform definition: add mf_contacts_contacts_subtable() to get definition for a _contacts subtable from categories
    git-svn-id: file:///Repos/modules/contacts/trunk@430 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

---
+++ zzform/definition.inc.php
@@ -0,0 +1,62 @@
+<?php 
+
+/**
+ * contacts module
+ * definition helper functions for forms with zzform
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/contacts
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+/**
+ * create _contacts-subtable from categories
+ *
+ * @param array $zz
+ * @param string $table prefix of table name *_contacts
+ * @param array $def definition of table
+ *		output of mf_default_categories_restrict()
+ *		int category_id
+ *		string category
+ *		string path
+ *		array parameters
+ * @param int $no
+ */
+function mf_contacts_contacts_subtable(&$zz, $table, $def, $no) {
+	$zz['fields'][$no] = zzform_include($table.'-contacts');
+	$zz['fields'][$no]['title'] = $def['category'];
+	$zz['fields'][$no]['table_name'] = $zz['fields'][$no]['table'].'_'.$def['category_id'];
+	$zz['fields'][$no]['type'] = 'subtable';
+	$zz['fields'][$no]['min_records'] = $def['parameters']['min_records'] ?? 1;
+	$zz['fields'][$no]['max_records'] = $def['parameters']['max_records'] ?? 20;
+	$zz['fields'][$no]['sql'] .= sprintf(' WHERE role_category_id = %d
+		ORDER BY sequence, contact', $def['category_id']);
+	$zz['fields'][$no]['form_display'] = 'lines';
+	$zz['fields'][$no]['fields'][2]['type'] = 'foreign_key';
+	$zz['fields'][$no]['fields'][3]['show_title'] = false;
+	$zz['fields'][$no]['fields'][3]['sql'] = sprintf('SELECT contact_id, contact
+		FROM contacts
+		LEFT JOIN categories
+			ON contacts.contact_category_id = categories.category_id
+		WHERE categories.parameters LIKE "%%&event_%s=1%%"
+		ORDER BY contact', $def['path']);
+	$zz['fields'][$no]['fields'][3]['add_details'] = $def['parameters']['add_details'] ?? false;
+	$zz['fields'][$no]['fields'][3]['select_dont_force_single_value'] = true;
+	$zz['fields'][$no]['fields'][4]['type'] = 'hidden';
+	$zz['fields'][$no]['fields'][4]['value'] = $def['category_id'];
+	$zz['fields'][$no]['fields'][4]['hide_in_form'] = true;
+	$zz['fields'][$no]['fields'][5]['type'] = 'sequence';
+	if (!empty($def['parameters']['role'])) {
+		$zz['fields'][$no]['fields'][6]['hide_in_form'] = false;
+		$zz['fields'][$no]['fields'][6]['placeholder'] = true;
+	}
+	$zz['fields'][$no]['class'] = 'hidden480';
+	$zz['fields'][$no]['unless']['export_mode']['subselect']['prefix'] = '<br><em>'.wrap_text($def['category']).'</em>: ';
+	$zz['fields'][$no]['unless']['export_mode']['subselect']['suffix'] = '';
+	$zz['fields'][$no]['unless']['export_mode']['list_append_next'] = true;
+	$zz['fields'][$no]['subselect']['sql'] .= sprintf(' WHERE role_category_id = %d', $def['category_id']);
+}
