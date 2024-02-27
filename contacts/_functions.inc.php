40dc72adcdb26f8928e2137d965527b70f03ce68
Gustaf Mossakowski <gustaf@koenige.org>
2024-02-27 16:16:35 +0000

    mf_contacts_addresses(): bugfix, allows now to fetch addresses of more than one contact
    git-svn-id: file:///Repos/modules/contacts/trunk@463 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- contacts/_functions.inc.php
+++ contacts/_functions.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2015, 2018, 2021-2023 Gustaf Mossakowski
+ * @copyright Copyright © 2015, 2018, 2021-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
