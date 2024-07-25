b5b2df4ce05d6cc581d346cc90e2137a334ba306
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-25 09:10:05 +0000

    zzform/definition: use wrap_edit_sql() to change queries
    git-svn-id: file:///Repos/modules/contacts/trunk@511 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzform/definition.inc.php
+++ zzform/definition.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @copyright Copyright © 2023-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -60,7 +60,10 @@ function mf_contacts_contacts_subtable(&$zz, $table, $def, $no) {
 		$zz['fields'][$no]['unless']['export_mode']['subselect']['suffix'] = '';
 		if (empty($def['last_category']))
 			$zz['fields'][$no]['unless']['export_mode']['list_append_next'] = true;
-		$zz['fields'][$no]['subselect']['sql'] .= sprintf(' WHERE role_category_id = %d', $def['category_id']);
+		$zz['fields'][$no]['subselect']['sql'] = wrap_edit_sql(
+			$zz['fields'][$no]['subselect']['sql'], 'WHERE',
+			sprintf('role_category_id = %d', $def['category_id'])	
+		);
 	}
 	$zz['fields'][$no]['hide_in_list'] = $def['parameters']['hide_in_list'] ?? false;
 }
