86e2fec4aca51679f1c3b38dccd4d7616fe989a9
Gustaf Mossakowski <gustaf@koenige.org>
2024-11-02 11:35:38 +0000

    contact placeholder: read contact_category_id, parameters, end_date per contact, too, parse category parameters and check for module parameters
    git-svn-id: file:///Repos/modules/contacts/trunk@517 9202a8f9-e154-40c8-a6c1-9c9c4ff3c01e

--- zzbrick_placeholder/contact.inc.php
+++ zzbrick_placeholder/contact.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2023 Gustaf Mossakowski
+ * @copyright Copyright © 2023-2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -19,6 +19,9 @@ function mod_contacts_placeholder_contact($brick) {
 	$sql = 'SELECT contact_id, contact, identifier
 			, SUBSTRING_INDEX(path, "/", -1) AS scope
 			, CONCAT("contact_id:", contacts.contact_id) AS contact_rights
+			, contact_category_id
+			, categories.parameters AS category_parameters
+			, end_date
 		FROM contacts
 		LEFT JOIN categories
 			ON contacts.contact_category_id = categories.category_id
@@ -26,6 +29,10 @@ function mod_contacts_placeholder_contact($brick) {
 	$sql = sprintf($sql, wrap_db_escape($brick['parameter']));
 	$brick['data'] = wrap_db_fetch($sql);
 	if (!$brick['data']) wrap_quit(404);
+	if ($brick['data']['category_parameters']) {
+		parse_str($brick['data']['category_parameters'], $brick['data']['category_parameters']);
+		wrap_match_module_parameters('contacts', $brick['data']['category_parameters'], false);
+	}
 
 	$zz_page['access'][] = $brick['data']['contact_rights'];
 	wrap_access_page($zz_page['db']['parameters'] ?? '', $zz_page['access']);
