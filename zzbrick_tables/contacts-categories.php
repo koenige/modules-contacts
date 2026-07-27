dedf594ca609fc7b9491f95b900cc9022683cc5c
Gustaf Mossakowski <gustaf@koenige.org>
2026-07-27 15:51:18 +0200

    translations: add Type as title for type_category_id

--- zzbrick_tables/contacts-categories.php
+++ zzbrick_tables/contacts-categories.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/contacts
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2009, 2016-2017, 2020, 2023-2025 Gustaf Mossakowski
+ * @copyright Copyright © 2009, 2016-2017, 2020, 2023-2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -45,6 +45,7 @@ $zz['fields'][3]['hide_in_list'] = true;
 
 $zz['fields'][4]['field_name'] = 'property';
 
+$zz['fields'][5]['title'] = 'Type';
 $zz['fields'][5]['field_name'] = 'type_category_id';
 $zz['fields'][5]['type'] = 'hidden';
 $zz['fields'][5]['type_detail'] = 'select';
